1cf26441aafd4b0f04390bbfbfd11cf2b5b9463d
weiwei.miao <weiwei.miao@sumscope.com>
2016-09-14 09:04:36 +0800

    基本 生成XML文件的功能 实现

--- WixPackageInstallTools/Form1.Designer.cs
+++ WixPackageInstallTools/Form1.Designer.cs
@@ -29,25 +29,36 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.FolderRbtn = new System.Windows.Forms.RadioButton();
+            this.FileRbtn = new System.Windows.Forms.RadioButton();
+            this.SourceBtn = new System.Windows.Forms.Button();
+            this.SourceTbx = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.TargetNam = new System.Windows.Forms.TextBox();
+            this.TargetBtn = new System.Windows.Forms.Button();
+            this.TargetTbx = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.CgTbx = new System.Windows.Forms.TextBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.CgBtn = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.FolderRbtn);
+            this.groupBox1.Controls.Add(this.FileRbtn);
+            this.groupBox1.Controls.Add(this.SourceBtn);
+            this.groupBox1.Controls.Add(this.SourceTbx);
             this.groupBox1.Location = new System.Drawing.Point(28, 37);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(422, 79);
@@ -55,82 +66,62 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "源文件/文件夹";
             // 
-            // textBox1
+            // FolderRbtn
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(6, 51);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(336, 21);
-            this.textBox1.TabIndex = 1;
+            this.FolderRbtn.AutoSize = true;
+            this.FolderRbtn.Location = new System.Drawing.Point(201, 29);
+            this.FolderRbtn.Name = "FolderRbtn";
+            this.FolderRbtn.Size = new System.Drawing.Size(59, 16);
+            this.FolderRbtn.TabIndex = 4;
+            this.FolderRbtn.Text = "文件夹";
+            this.FolderRbtn.UseVisualStyleBackColor = true;
+            this.FolderRbtn.CheckedChanged += new System.EventHandler(this.FolderRbtn_CheckedChanged);
             // 
-            // button1
+            // FileRbtn
             // 
-            this.button1.Location = new System.Drawing.Point(366, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "选择";
-            this.button1.UseVisualStyleBackColor = true;
+            this.FileRbtn.AutoSize = true;
+            this.FileRbtn.Checked = true;
+            this.FileRbtn.Location = new System.Drawing.Point(83, 29);
+            this.FileRbtn.Name = "FileRbtn";
+            this.FileRbtn.Size = new System.Drawing.Size(47, 16);
+            this.FileRbtn.TabIndex = 3;
+            this.FileRbtn.TabStop = true;
+            this.FileRbtn.Text = "文件";
+            this.FileRbtn.UseVisualStyleBackColor = true;
+            this.FileRbtn.CheckedChanged += new System.EventHandler(this.FileRbtn_CheckedChanged);
             // 
-            // radioButton1
+            // SourceBtn
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(83, 29);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(47, 16);
-            this.radioButton1.TabIndex = 3;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "文件";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.SourceBtn.Location = new System.Drawing.Point(366, 51);
+            this.SourceBtn.Name = "SourceBtn";
+            this.SourceBtn.Size = new System.Drawing.Size(50, 23);
+            this.SourceBtn.TabIndex = 2;
+            this.SourceBtn.Text = "选择";
+            this.SourceBtn.UseVisualStyleBackColor = true;
+            this.SourceBtn.Click += new System.EventHandler(this.SourceBtn_Click);
             // 
-            // radioButton2
+            // SourceTbx
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(201, 29);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(59, 16);
-            this.radioButton2.TabIndex = 4;
-            this.radioButton2.Text = "文件夹";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.SourceTbx.Location = new System.Drawing.Point(6, 51);
+            this.SourceTbx.Multiline = true;
+            this.SourceTbx.Name = "SourceTbx";
+            this.SourceTbx.ReadOnly = true;
+            this.SourceTbx.Size = new System.Drawing.Size(336, 21);
+            this.SourceTbx.TabIndex = 1;
+            this.SourceTbx.WordWrap = false;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.textBox3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.TargetNam);
+            this.groupBox2.Controls.Add(this.TargetBtn);
+            this.groupBox2.Controls.Add(this.TargetTbx);
             this.groupBox2.Location = new System.Drawing.Point(28, 122);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(422, 83);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "目标文件/文件夹";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(7, 20);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(335, 21);
-            this.textBox2.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(366, 20);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "选择";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(90, 49);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(328, 21);
-            this.textBox3.TabIndex = 2;
             // 
             // label1
             // 
@@ -142,11 +133,129 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "目标文件名";
             // 
+            // TargetNam
+            // 
+            this.TargetNam.Location = new System.Drawing.Point(90, 49);
+            this.TargetNam.Name = "TargetNam";
+            this.TargetNam.Size = new System.Drawing.Size(328, 21);
+            this.TargetNam.TabIndex = 2;
+            // 
+            // TargetBtn
+            // 
+            this.TargetBtn.Location = new System.Drawing.Point(366, 20);
+            this.TargetBtn.Name = "TargetBtn";
+            this.TargetBtn.Size = new System.Drawing.Size(50, 23);
+            this.TargetBtn.TabIndex = 1;
+            this.TargetBtn.Text = "选择";
+            this.TargetBtn.UseVisualStyleBackColor = true;
+            this.TargetBtn.Click += new System.EventHandler(this.TargetBtn_Click);
+            // 
+            // TargetTbx
+            // 
+            this.TargetTbx.Enabled = false;
+            this.TargetTbx.Location = new System.Drawing.Point(7, 20);
+            this.TargetTbx.Name = "TargetTbx";
+            this.TargetTbx.ReadOnly = true;
+            this.TargetTbx.Size = new System.Drawing.Size(335, 21);
+            this.TargetTbx.TabIndex = 0;
+            this.TargetTbx.WordWrap = false;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(633, 37);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 3;
+            this.StartBtn.Text = "开始";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox5);
+            this.groupBox3.Controls.Add(this.CgTbx);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.CgBtn);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Location = new System.Drawing.Point(28, 230);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(418, 130);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "参数选择";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 108);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(168, 16);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "为每个组件生成唯一表示Id";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // CgTbx
+            // 
+            this.CgTbx.Enabled = false;
+            this.CgTbx.Location = new System.Drawing.Point(170, 44);
+            this.CgTbx.Name = "CgTbx";
+            this.CgTbx.Size = new System.Drawing.Size(100, 21);
+            this.CgTbx.TabIndex = 4;
+            this.CgTbx.WordWrap = false;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 87);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(132, 16);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "为每个组件生产guid";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 67);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(132, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "只生成一个Fragment";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // CgBtn
+            // 
+            this.CgBtn.AutoSize = true;
+            this.CgBtn.Location = new System.Drawing.Point(7, 44);
+            this.CgBtn.Name = "CgBtn";
+            this.CgBtn.Size = new System.Drawing.Size(156, 16);
+            this.CgBtn.TabIndex = 1;
+            this.CgBtn.Text = "设置ComponentGroup名称";
+            this.CgBtn.UseVisualStyleBackColor = true;
+            this.CgBtn.CheckedChanged += new System.EventHandler(this.CgBtn_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "自动生成组件Guid[*]";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(720, 396);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.StartBtn);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -155,6 +264,8 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -162,15 +273,25 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button SourceBtn;
+        private System.Windows.Forms.TextBox SourceTbx;
+        private System.Windows.Forms.RadioButton FolderRbtn;
+        private System.Windows.Forms.RadioButton FileRbtn;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TargetNam;
+        private System.Windows.Forms.Button TargetBtn;
+        private System.Windows.Forms.TextBox TargetTbx;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox CgTbx;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox CgBtn;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
     }
 }
 
--- WixPackageInstallTools/Form1.cs
+++ WixPackageInstallTools/Form1.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,10 +13,124 @@ namespace WixPackageInstallTools
 {
     public partial class Form1 : Form
     {
+        private string cmd = "heat";
+        private string harvestType;//得到类型
+        private string soucerFilePath;//源路径
+        private string arguments;//参数
+        private string targetFilePath;//目标路径
         public Form1()
         {
             InitializeComponent();
-            
+
+        }
+
+        private void FileRbtn_CheckedChanged(object sender, EventArgs e)
+        {
+            SourceTbx.Text = "";
+        }
+        private void FolderRbtn_CheckedChanged(object sender, EventArgs e)
+        {
+            SourceTbx.Text = "";
+        }
+        /// <summary>
+        /// 选中源文件/文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SourceBtn_Click(object sender, EventArgs e)
+        {
+            if (FileRbtn.Checked)
+            {
+                openFileDialog1.Multiselect = false; //是否允许选中多个文件
+                openFileDialog1.Title = "请选择文件";
+                openFileDialog1.Filter = "所有文件(*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    harvestType = "file";
+                    string file = openFileDialog1.FileName;
+                    SourceTbx.Text = file;
+                    soucerFilePath = file;
+                    //MessageBox.Show("已选择文件:" + file, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (FolderRbtn.Checked)
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    harvestType = "dir";
+                    SourceTbx.Text = folderBrowserDialog1.SelectedPath;
+                    soucerFilePath = folderBrowserDialog1.SelectedPath;
+                }
+            }
+        }
+        /// <summary>
+        /// 选中目标文件/文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TargetBtn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TargetTbx.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 开始执行heat.exe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (SourceTbx.Text == "" || TargetTbx.Text == "" || TargetNam.Text == "")
+            {
+                MessageBox.Show("请先选好源文件/文件夹和目标文件夹并填写目标文件的名字，然后在点击开始", "提示");
+                return;
+            }
+          
+            if (FileRbtn.Checked)
+            {
+                cmd = cmd + harvestType + soucerFilePath.Trim() + arguments + " -out " + targetFilePath.Trim() + TargetNam.Text.Trim();
+            }
+            else if (FolderRbtn.Checked)
+            {
+                cmd = cmd + harvestType + soucerFilePath.Trim() + " -out " + targetFilePath.Trim() + TargetNam.Text.Trim();
+            }
+
+            string output = null;
+            cmd = cmd.Trim().TrimEnd('&') + "&exit"; //说明：不管命令是否成功均执行exit命令，否则当调用ReadToEnd()方法时，会处于假死状态
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = @"cmd.exe";   //执行exe的路径
+                p.StartInfo.UseShellExecute = false; //是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true; //接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true; //由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true; //重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true; //不显示程序窗口
+                p.Start(); //启动程序
+
+                //向cmd窗口写入命令
+                p.StandardInput.WriteLine(cmd);
+                p.StandardInput.AutoFlush = true;
+
+                //获取cmd窗口的输出信息
+                output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit(); //等待程序执行完退出进程
+                p.Close();
+            }
+        }
+
+        private void CgBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CgBtn.Checked)
+            {
+                CgTbx.Enabled = true;
+            }
+            else
+            {
+                CgTbx.Enabled = false;
+            }
         }
     }
+
 }
